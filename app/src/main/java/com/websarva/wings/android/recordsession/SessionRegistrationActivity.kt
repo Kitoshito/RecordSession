b97671b7904559403edebd3afe976c7eebc2dc11
KittoSit <44728761+KittoSit@users.noreply.github.com>
2018-11-13 23:26:22 +0900

    ky:動かないけどPLCODE1が入力されたときにPLCODE2を表示する処理書いた

--- app/src/main/java/com/websarva/wings/android/recordsession/SessionRegistrationActivity.kt
+++ app/src/main/java/com/websarva/wings/android/recordsession/SessionRegistrationActivity.kt
@@ -5,6 +5,8 @@ import android.app.Dialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
 import android.widget.DatePicker
@@ -17,8 +19,10 @@ import java.util.*
 /**
  * スタートアップオブジェクト
  */
-class SessionRegistrationActivity : AppCompatActivity() {
+class SessionRegistrationActivity : AppCompatActivity(), TextWatcher {
 
+    private var tvPlCode2: TextView? = null
+    private var etPlCode2: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,13 +33,47 @@ class SessionRegistrationActivity : AppCompatActivity() {
             finish()
         }
 
-        //日付ダイアログの表示
-        val textView = findViewById(R.id.tw_start_time) as TextView
+        //日付ダイアログを表示して値を取得
+        val textView = findViewById<TextView>(R.id.tw_start_time)
         textView.setOnClickListener {
 
             val datePicker = DatePickerDialogFragment()
             datePicker.show(supportFragmentManager, "datePicker")
         }
+
+        tvPlCode2 = findViewById(R.id.tv_pl_code2)
+        etPlCode2 = findViewById(R.id.et_pl_code2)
+
+        //afterTextChangedのためのやーつ
+        val plCode1 = findViewById<EditText>(R.id.et_pl_code1)
+        plCode1.addTextChangedListener(this)
+
+    }
+
+    //値が入ればEditTextを表示したい
+    override fun afterTextChanged(p0: Editable?) {
+
+        if(p0 == null) {
+            print("p0")
+            return
+        }
+        val inputStr = p0.toString()
+        if(inputStr == null) {
+            print("inputStr")
+            return
+        }
+        val inputLength = inputStr.length
+        if(inputLength > 0 && tvPlCode2 != null && etPlCode2 != null) {
+            tvPlCode2?.visibility = View.VISIBLE
+            etPlCode2?.visibility = View.VISIBLE
+        }
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
 
@@ -55,7 +93,7 @@ class DatePickerDialogFragment : DialogFragment(), DatePickerDialog.OnDateSetLis
 
     override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
 
-        val textView = activity!!.findViewById(R.id.tw_start_time) as TextView
+        val textView = activity!!.findViewById<TextView>(R.id.tw_start_time)
         textView.text = year.toString()+ "年" + month.toString() + "月" + day.toString() + "日"
     }
 }
